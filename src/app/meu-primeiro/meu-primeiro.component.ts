67edb5ced4dc7a20093fcca9ce635eee04c205b7
Lucas Machado <lucas.machado290@gmail.com>
2020-05-14 19:47:27 -0300

    add hello world

---
+++ src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'meu-primeiro',
+  templateUrl: './meu-primeiro.component.html',
+  styleUrls: ['./meu-primeiro.component.css']
+})
+export class MeuPrimeiroComponent  {
+
+}
