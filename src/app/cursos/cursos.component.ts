6eed21c933018e4b9d2625118b1ce3125a809e68
Lucas Machado <lucas.machado290@gmail.com>
2020-05-14 21:20:30 -0300

    add services and DI exercises

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -1,21 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CursosService } from './cursos.service';
+
 @Component({
   selector: 'app-cursos',
   templateUrl: './cursos.component.html',
   styleUrls: ['./cursos.component.css']
 })
-export class CursosComponent implements OnInit {
+export class CursosComponent {
 
   nomePortal: string;
 
-  cursos: string[] = ['Java', 'Ext JS', 'Angular'];
+  cursos: string[];
 
-  constructor() {
+  constructor(private cursosService: CursosService) { 
     this.nomePortal = 'https://loiane.training/';
-  }
 
-  ngOnInit() {
+    this.cursos = this.cursosService.getCursos();
   }
 
 }
